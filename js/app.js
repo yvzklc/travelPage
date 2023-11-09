333534f764dac3092fefdf36a23faade5a47aa90
yvzklc <kilciyavuz34@gmail.com>
2023-11-09 23:38:51 +0300

    nav finished

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-//? JS
+
 const hamburgerBtn = document.querySelector(".fa-bars")
 
-hamburgerBtn.addEventListener("click",()=> {
-    const headerLinks = document.querySelector(".header_links")
+    hamburgerBtn.addEventListener("click", ()=>{
+    const headerLinks = document.querySelector(".header__links")
     headerLinks.classList.toggle("show")
+    // headerlinksin classlistine show'u ekle.
+
 })
