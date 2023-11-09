5cc2700a94f8243e582eb016fe8b66bedf378757
yvzklc <kilciyavuz34@gmail.com>
2023-11-09 23:28:25 +0300

    some changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 //? JS
+const hamburgetBtn = document.querySelector(".fa-bars")
+
+hamburgetBtn.addEventListener("click",()=> {
+    const headerLinks = document.querySelector(".header_links")
+    headerLinks.classList.toggle("show")
+})
